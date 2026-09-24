b5329582a6f26fbbaa28081c7ec8b1ba8d3bbc48
DiullyCarvalho <diully.carvalho@estudante.ifgoiano.edu.br>
2026-09-24 08:17:23 -0300

    Cad do usuario

---
+++ cad_usuario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cad usuario</title>
+</head>
+<body>
+    <h3>Cadastro de Curso</h3>
+
+    <form action="sal_usuario.php" method="GET">
+        Username: <br>
+        <input type="text" name="username"> <br>
+        
+        Nome: <br>
+        <input type="text" name="nome"> <br>
+        
+        Email: <br>
+        <input type="email" name="email"> <br>
+
+        Senha: <br>
+        <input type="password" name="senha"> <br>
+
+        Foto: <br>
+        <input type="text" name="foto"> <br>
+
+        <input type="submit" value="Cadastrar">
+        <a href="index.php">Cancelar</a>
+    </form>
+</body>
+</html>
+
+    
